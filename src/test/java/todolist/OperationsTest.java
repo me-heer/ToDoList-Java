73302430a968ac7423428427e2abb15fe0d83ab5
Mihir Joshi <mihir67mj@gmail.com>
2019-12-18 09:07:17 +0530

    Added Test

--- src/test/java/todolist/OperationsTest.java
+++ src/test/java/todolist/OperationsTest.java
@@ -1,14 +1,13 @@
 package todolist;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.junit.Test;
 
 public class OperationsTest{
+    @Test
     public void addTaskTest(){
         ArrayList<ToDoList> testToDo = new ArrayList<ToDoList>();
         assertEquals(0, testToDo.size());
